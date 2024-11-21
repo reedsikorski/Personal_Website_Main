cfd2d2b3760e133ed48d4c10ca306bf30705a656
reedsikorski <60444882+reedsikorski@users.noreply.github.com>
2024-11-21 10:55:49 -0500

    update 10:55am

--- myjs.js
+++
@@ -1,8 +0,0 @@
-window.onSpotifyIframeApiReady = (IFrameAPI) => {
-  const element = document.getElementById("embed-iframe");
-  const options = {
-    uri: "spotify:episode:7makk4oTQel546B0PZlDM5",
-  };
-  const callback = (EmbedController) => {};
-  IFrameAPI.createController(element, options, callback);
-};
