3ed66d0b23f228f521699df31d004853afb11f56
reedsikorski <60444882+reedsikorski@users.noreply.github.com>
2024-12-04 13:24:37 -0500

    WORKING JS API CALL BY CITY
    LFG it works

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const apiKey = "50be8ea5d271b0daabdfd3c527199276";
-const apiUrl = "https://api.openweathermap.com/data/2.5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 const locationInput = document.getElementById("locationInput");
 const searchButton = document.getElementById("searchButton");
@@ -15,14 +15,23 @@ searchButton.addEventListener("click", () => {
   }
 });
 
-function fetchWeather(location) {
-  const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+async function fetchWeather(location) {
+  const url = `${apiUrl}?q=${location}&units=imperial&appid=${apiKey}`;
   console.log(url);
-  fetch(url)
-    .then((weather) => weather.json())
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-    });
+
+  try {
+    console.log("HERE1");
+    const wresponse = await fetch(url);
+    if (!wresponse.ok) {
+      throw new Error(`HTTP error! status: ${wresponse.status}`);
+    }
+    const weatherInfo = await wresponse.json();
+    console.log(weatherInfo);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-function displayWeather(weather) {}
+function displayWeather(weather) {
+  console.log(weather);
+}
